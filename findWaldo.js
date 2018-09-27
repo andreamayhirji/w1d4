77a15063c28d783c0ff93c930b2376ee3b38c2df
Andrea <andreamayhirji@gmail.com>
2018-09-27 18:08:04 +0000

    created a findWaldo.js

---
+++ findWaldo.js
@@ -0,0 +1,18 @@
+//callBacks
+
+// are, found  -- these are parameters
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found();   // execute callback
+    }
+  }
+}
+
+function actionWhenFound() {
+  console.log("Found him!");
+}
+
+// ["Alice", "Bob", "Waldo", "Winston"] and actionWhenFound -- these are arguments
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
