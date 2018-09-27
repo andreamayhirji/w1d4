283ec81d27756ce7fd3c6f2e5b8f920fd3d37f35
Andrea <andreamayhirji@gmail.com>
2018-09-27 19:46:02 +0000

    refactored the findWaldo.js code to use a forEach method

--- findWaldo_refactored.js
+++ findWaldo_refactored.js
@@ -1,21 +1,23 @@
 //using forEach
 
 // are, found  -- these are parameters
-function findWaldo.forEach(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found([i]);   // execute callback
+
+function findWaldo(arr, cb) {
+//                      these need to be in this order for forEach to run.
+  arr.forEach(function (element, i, list) {
+    if (element === "Waldo") {
+      cb(i);
     }
-  }
+  })
 }
 
-function actionWhenFound([i]) {
-  console.log("Found Waldo at index " + [i]) ;
+function actionWhenFound(i) {
+  console.log("Found Waldo at index " + i) ;
 }
 
 // ["Alice", "Bob", "Waldo", "Winston"] and actionWhenFound -- these are arguments
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+findWaldo(["Alice", "Bob", "mary", "Waldo", "Winston"], actionWhenFound);
 
 
 
