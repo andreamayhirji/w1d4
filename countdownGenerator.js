c442b52cb68d0150ea2dc0d2a85139983faf160f
Andrea <andreamayhirji@gmail.com>
2018-09-28 02:43:58 +0000

    solved the problem.

--- countdownGenerator.js
+++ countdownGenerator.js
@@ -2,10 +2,22 @@
 
 
 var countdownGenerator = function (x) {
-  /* your code here */
-};
+
+  return function() {
+
+    if (x > 0) {
+      console.log('T-minus ' + x + '...');
+    } else if (x == 0) {
+      console.log('Blast Off!');
+    } else if (x < 0) {
+      console.log('Rockets already gone, bub!');
+    }
+    x = x - 1;
+  }
+}
 
 var countdown = countdownGenerator(3);
+
 countdown(); // T-minus 3...
 countdown(); // T-minus 2...
 countdown(); // T-minus 1...
