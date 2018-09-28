6d1a6d9c95896e51892c9d4801e7bcfac13b7e7b
Andrea <andreamayhirji@gmail.com>
2018-09-28 01:33:57 +0000

    solution to loaded die problem

---
+++ loadedDie.js
@@ -0,0 +1,26 @@
+// create a cheat die that would let us predict the next number.
+//Create a function called makeLoadedDie() which returns a function that when called, seems to generate random numbers between 1 and 6, but in fact returns numbers in a row from a hardcoded list.
+
+
+
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+
+var counter = -1;
+
+  return function() {
+    /* your code here */
+    counter += 1;
+    return list[counter]
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
